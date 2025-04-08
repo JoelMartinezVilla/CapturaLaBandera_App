6b588251a69f49c1d7c5f7442af0987def851a2e
JoelMartinezVilla <joelmv2004@gmail.com>
2025-04-08 18:12:50 +0200

    Add connection to websockets

--- core/src/main/java/io/github/capturdebander/Main.java
+++ core/src/main/java/io/github/capturdebander/Main.java
@@ -2,12 +2,11 @@ package io.github.capturdebander;
 
 import com.badlogic.gdx.Game;
 
-
 public class Main extends Game {
 
     @Override
     public void create() {
-        setScreen(new MainMenuScreen());
+        // Se pasa "this" para poder cambiar de pantalla desde MainMenuScreen
+        setScreen(new MainMenuScreen(this));
     }
-
 }
--- core/src/main/java/io/github/capturdebander/MainMenuScreen.java
+++ core/src/main/java/io/github/capturdebander/MainMenuScreen.java
@@ -1,37 +1,50 @@
 package io.github.capturdebander;
 
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 public class MainMenuScreen implements Screen {
-
     private Stage stage;
     private Skin skin;
+    private Game game; // Referencia al juego principal
 
-    public MainMenuScreen() {
+    public MainMenuScreen(Game game) {
+        this.game = game;
         // Inicializamos el Stage con un viewport
         stage = new Stage(new ScreenViewport());
         // Establecemos el stage como procesador de entrada
         Gdx.input.setInputProcessor(stage);
 
-        // Cargamos un Skin. Puedes usar el "uiskin.json" que viene en los ejemplos de LibGDX.
+        // Cargamos el Skin
         skin = new Skin(Gdx.files.internal("uiskin.json"));
 
-        // Creamos una tabla para organizar los elementos de forma sencilla.
+        // Creamos una tabla para organizar los elementos
         Table table = new Table();
         table.setFillParent(true);
         table.center();
 
         // Creamos el título y el botón
         Label titleLabel = new Label("Captura la bandera", skin);
-        TextButton dummyButton = new TextButton("Botón", skin);
+        TextButton dummyButton = new TextButton("Ir a GameScreen", skin);
+
+        // Agregamos un listener para detectar el clic en el botón
+        dummyButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                // Al pulsar el botón, cambiamos la pantalla a GameScreen
+                game.setScreen(new GameScreen());
+            }
+        });
 
         // Agregamos los elementos a la tabla
         table.add(titleLabel).padBottom(20);
@@ -49,7 +62,7 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        // Limpiamos la pantalla con un color (en este caso negro)
+        // Limpiamos la pantalla con un color negro
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
